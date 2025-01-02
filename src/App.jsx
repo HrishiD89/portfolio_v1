6040ca99b6789bba99c529fec2587bfc9dc6ad3f
Hrishikesh Kalita <95520030+HrishiD89@users.noreply.github.com>
2025-01-02 19:03:04 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className=" w-full gap-2 p-8 h-screen flex flex-col justify-center items-center bg-zinc-800">
+      <h1 className=" text-zinc-600 text-5xl font-semibold">React + JS + Tailwind Starter code</h1>
+      <h2 className="text-semibold text-zinc-600 text-xl">Edit <code>src/App.jsx</code> and save to reload.</h2>
+      <h2 className="text-semibold text-zinc-600 text-xl">Remove this paragraph and replace with your own component</h2>
+    </div>
+  )
+}
